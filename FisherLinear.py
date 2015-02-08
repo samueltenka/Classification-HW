f35aeb90b9a59394ccf0ae4e7889f0ffb2d1d950
samueltenka <iguavadon@gmail.com>
2015-02-07 21:25:50 -0500

    Update FisherLinear.py
    With Manoj's sigma/mu estimate of offset, giving preference toward more sharply clustered classes.

--- FisherLinear.py
+++ FisherLinear.py
@@ -19,15 +19,21 @@ def compute_params(xts):
     mus = {t:sum(C[t])/len(C[t]) for t in [0,1]}
     S = sum(outer(x-mus[t], x-mus[t]) for x,t in xts)
     weights = dot(inv(S), (mus[1]-mus[0]))
-    offset = vdot(weights, (mus[1]+mus[0])/2)
-    return weights, offset*1.0
+    normed = weights / vdot(weights,weights)**0.5
+
+    # projected:
+    pxs = {t:[vdot(normed, x) for x in C[t]] for t in [0, 1]}
+    pmus = {t:sum(pxs[t])/len(pxs[t]) for t in [0, 1]}
+    pvars = {t:(sum((px-pmus[t])**2 for px in pxs[t])/len(pxs[t]))**0.5 for t in [0, 1]}
+    offset = (pvars[1]*pmus[0]+pvars[0]*pmus[1])/(pvars[0]+pvars[1]) #vdot(weights, (mus[1]+mus[0])/2)
+    return normed, offset
 
 def generate_boundary(weights, offset, x1_range):
     x1s = [x1 for (x1,x2),t in xts]
     x1_range = 1.5*(max(x1s)-min(x1s))
 
     x1s = [(n-50)*x1_range/100.0 for n in range(100)]
-    x2s = [(-offset-weights[0]*x1)/weights[1] for x1 in x1s]
+    x2s = [(offset-weights[0]*x1)/weights[1] for x1 in x1s]
     return x1s, x2s
 
 
